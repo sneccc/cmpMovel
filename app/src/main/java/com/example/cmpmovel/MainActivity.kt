88b077cdda72f7f509fdacd5bf9bd929f863274d
Daniel Gonçalves <daniell5984@gmail.com>
2021-04-01 14:14:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cmpmovel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cmpmovel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  // Associação da atividade com o layout
+    }
+}
