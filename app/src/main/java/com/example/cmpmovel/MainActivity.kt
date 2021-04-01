ff19675f904f22b59ad77680b828fa3e3c555007
Daniel Gonçalves <daniell5984@gmail.com>
2021-04-01 14:57:22 +0100

    teste2

--- app/src/main/java/com/example/cmpmovel/MainActivity.kt
+++ app/src/main/java/com/example/cmpmovel/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)  // Associação da atividade com o  layout
+        setContentView(R.layout.activity_main)  // Associação da atividade com o  layout (Testar commit)
     }
 }
