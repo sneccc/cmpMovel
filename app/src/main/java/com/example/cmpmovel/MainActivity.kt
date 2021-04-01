09d9d9640e03595acb6fe0c72e715edeb201de1d
Daniel Gonçalves <daniell5984@gmail.com>
2021-04-01 23:29:24 +0100

    Modificações layout

--- app/src/main/java/com/example/cmpmovel/MainActivity.kt
+++ app/src/main/java/com/example/cmpmovel/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.cmpmovel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)  // Associação da atividade com o  layout (Testar commit)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Log.d( "consoleTAG", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d( "consoleTAG", "onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d( "consoleTAG", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d( "consoleTAG", "onResume")
+    }
+
+    override fun onDestroy() {
+            super.onDestroy()
+        Log.d( "consoleTAG", "onDestroy")
+    }
+
+
+    fun adicionar(view: View) {
+        var editText = findViewById<EditText>(R.id.editTextName)
+        Log.d( "consoleTAG", editText.text.toString())
+
+
+
     }
 }
