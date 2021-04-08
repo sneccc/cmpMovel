b6c56515a7da847ab19194ec14a97050cdc24364
Daniel Gonçalves <daniell5984@gmail.com>
2021-04-08 11:06:15 +0100

    Modificações na activity da pagina inicial

---
+++ app/src/main/java/com/example/cmpmovel/PaginaInicial.kt
@@ -0,0 +1,11 @@
+package com.example.cmpmovel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaginaInicial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina_inicial)
+    }
+}
